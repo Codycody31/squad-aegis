ef846e3a64a589346b4ebe7a7f85035517f9593b
Insidious Fiddler <me@codycody31.dev>
2025-09-08 23:48:11 -0400

    feat: add Discord Round Winner plugin to send round winner information to Discord

--- internal/plugin_registry/registry.go
+++ internal/plugin_registry/registry.go
@@ -14,6 +14,7 @@ import (
 	"go.codycody31.dev/squad-aegis/internal/plugins/discord_chat"
 	"go.codycody31.dev/squad-aegis/internal/plugins/discord_fob_hab_explosion_damage"
 	"go.codycody31.dev/squad-aegis/internal/plugins/discord_kill_feed"
+	"go.codycody31.dev/squad-aegis/internal/plugins/discord_round_winner"
 	"go.codycody31.dev/squad-aegis/internal/plugins/fog_of_war"
 	"go.codycody31.dev/squad-aegis/internal/plugins/intervalled_broadcasts"
 	"go.codycody31.dev/squad-aegis/internal/plugins/seeding_mode"
@@ -81,6 +82,12 @@ func RegisterAllPlugins(pm *plugin_manager.PluginManager) error {
 	// Register Discord Kill Feed plugin
 	if err := pm.RegisterPlugin(discord_kill_feed.Define()); err != nil {
 		log.Error().Err(err).Msg("Failed to register Discord Kill Feed plugin")
+		return err
+	}
+
+	// Register Discord Round Winner plugin
+	if err := pm.RegisterPlugin(discord_round_winner.Define()); err != nil {
+		log.Error().Err(err).Msg("Failed to register Discord Round Winner plugin")
 		return err
 	}
 
