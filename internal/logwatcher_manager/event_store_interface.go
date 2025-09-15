a001469c1c64ec996e002d7403ac902dcdc5effe
Insidious Fiddler <me@codycody31.dev>
2025-09-15 17:28:46 -0400

    chore: fix interface

--- internal/logwatcher_manager/event_store_interface.go
+++ internal/logwatcher_manager/event_store_interface.go
@@ -17,14 +17,11 @@ type EventStoreInterface interface {
 	// Player data methods
 	StorePlayerData(playerID string, data *PlayerData)
 	GetPlayerData(playerID string) (*PlayerData, bool)
-	RemovePlayerData(playerID string) bool
+	RemovePlayerData(playerID string) error
 
 	// Session data methods
 	StoreSessionData(key string, data *SessionData)
 	GetSessionData(key string) (*SessionData, bool)
-
-	// Disconnected player methods
-	RemoveDisconnectedPlayer(playerID string)
 
 	// Round data methods
 	StoreRoundWinner(data *RoundWinnerData)
