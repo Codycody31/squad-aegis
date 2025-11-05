3284715c267793e19a77450a3c951e492c6cf0f9
Insidious Fiddler <me@codycody31.dev>
2025-11-05 12:36:27 -0500

    docs: v2 via fumadocs

---
+++ docs/app/(home)/layout.tsx
@@ -0,0 +1,12 @@
+import { HomeLayout } from 'fumadocs-ui/layouts/home';
+import { baseOptions } from '@/lib/layout.shared';
+
+export default function Layout({ children }: LayoutProps<'/'>) {
+  return <HomeLayout {...baseOptions()}     links={[
+    {
+      text: 'Documentation',
+      url: '/docs',
+      secondary: false,
+    },
+  ]}>{children}</HomeLayout>;
+}
